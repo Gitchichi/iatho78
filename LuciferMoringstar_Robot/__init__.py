f398ac67a903749cd100b39e6f11d6ca0e3ea6f2
Gitchichi <108982155+Gitchichi@users.noreply.github.com>
2022-08-12 19:17:45 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,15 @@ Bot Owner Only
 """
 
 ABOUT = """
+
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Torc⚡️]
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : [Click Me](https://youtu.be/5hngYOKBzyi8)
+
 """
